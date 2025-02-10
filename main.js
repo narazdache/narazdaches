3edc33c8ab1fe5a1f336db4b245803b6fabf90b8
narazdache <nestortrotskii@gmail.com>
2025-02-10 17:58:43 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("number:", typeof 42);              
+console.log("bigint:", typeof 9007199254740991n); 
+console.log("boolean:", typeof true);           
+console.log("undefined:", typeof undefined);   
+console.log("null:", typeof null);             
+console.log("symbol:", typeof Symbol("id"));   
+console.log("object:", typeof { name: "nestor", age: 16 }); 
+console.log("array:", typeof [1, 2, 3]);       
+console.log("function:", typeof function(){});
